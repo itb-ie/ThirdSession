c0c545140c62810c1696b16d4eecab1de3334fef
Bogdan Ratiu <bratiu-dn@drivenets.com>
2023-01-26 10:59:45 +0100

    this is the third session class example

---
+++ print_examples.py
@@ -0,0 +1,5 @@
+print("This is a text", "another text", end="|I want this at the end|\n")
+print("this is another")
+print("this", 2+2, "be", "written", "like", "this")
+# this is a not so useful comment
+print("this", 2+2, "be", "written", "like", "this", sep="!b ?! ")
